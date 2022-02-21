1966ce2ed074256a3378cf47d6ec6555129f99a1
Evanticks <pl2011_50@hotmail.com>
2022-02-21 12:49:00 +0100

    Dark_Souls.sql

--- Dark_Souls.sql
+++ Dark_Souls.sql
@@ -1,28 +1,27 @@
 CREATE TABLE personaje (
-    codpersonaje decimal (3),
-    nombre varchar (15),
-    altura decimal (3,2),
-    peso decimal (3),
-    raza varchar (10),
-    codarma decimal (3),
-    CONSTRAINT pk_codpersonaje PRIMARY KEY (codpersonaje)
+codpersonaje varchar (3),
+nombre varchar (15),
+altura decimal (3,2),
+peso decimal (3),
+raza varchar (10),
+CONSTRAINT pk_codpersonaje PRIMARY KEY (codpersonaje)
 );
 
 CREATE TABLE equipar (
-    codpersonaje decimal (3),
-    codarma decimal (3),
-    fecha date,
-    CONSTRAINT pk_equipar PRIMARY KEY (codpersonaje,codarma,fecha),
-    CONSTRAINT fk_equipar FOREIGN KEY 
+codpersonaje varchar (3),
+codarma varchar (3),
+fecha date,
+CONSTRAINT pk_equipar PRIMARY KEY (codpersonaje,codarma,fecha),
+CONSTRAINT fk_codpersonaje FOREIGN KEY (codpersonaje) REFERENCES personaje (codpersonaje)
 );
 
 CREATE TABLE armas (
-    codarma decimal (3),
-    fuerza decimal (2),
-    destreza decimal (2),
-    inteligencia decimal (2),
-    rareza varchar (10),
-    durabilidad decimal (3),
-    nivel decimal (2),
-    CONSTRAINT pk_armas PRIMARY KEY (codarmas)
+codarma varchar (3),
+fuerza decimal (2),
+destreza decimal (2),
+inteligencia decimal (2),
+rareza varchar (10),
+durabilidad decimal (3),
+nivel decimal (2),
+CONSTRAINT pk_armas PRIMARY KEY (codarma)
 );
